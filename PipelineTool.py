1a5b45f545cd9b4122fe6646acef05a058393e45
panda <damgteam@gmail.com>
2017-10-28 13:34:57 +0100

    redo login

--- PipelineTool.py
+++ PipelineTool.py
@@ -6,9 +6,14 @@ Author: Do Trinh/Jimmy - 3D artist.
 Description:
     This script will start some installation then call the main UI of the apps.
 """
+
+
+
 import os, sys, logging, subprocess, json, shutil
 from tk import appFuncs as func
 from tk import autoUpdate as update
+
+print 'it works here'
 
 logging.basicConfig()
 logger = logging.getLogger(__file__)
@@ -17,6 +22,8 @@ logger.setLevel(logging.DEBUG)
 key = 'PIPELINE_TOOL'
 toolName = 'Pipeline Tool'
 scrInstall = os.getenv('PROGRAMDATA')
+
+print 'it works here'
 
 func.checkEnvKey(key, scrInstall, toolName)
 
@@ -32,6 +39,8 @@ userSetupScr = os.path.join(os.getcwd(), 'Maya_tk/userSetup.py')
 userSetupDes = os.path.join(os.path.expanduser('~/Documents/maya/2017/prefs/scripts'), 'userSetup.py')
 
 shutil.copy2(userSetupScr, userSetupDes)
+
+
 
 #login UI
 from ui import DesktopUI
