b55dcaab85ec34b3b48cc86bc08513b78220837d
panda <damgteam@gmail.com>
2017-11-23 10:52:02 +1300

    update code

---
+++ Maya_tk/modules/randomizer.py
@@ -0,0 +1,38 @@
+from maya import cmds
+import random
+
+def createObjects(mode, numObjects):
+    """
+    This creates objects. Support Cubes, Spheres, Cylinders and Cones
+    :param mode:
+    :param numObjects:
+    :return:
+    """
+
+    objList = []
+
+    for n in range(numObjects):
+        if mode == 'Cube':
+            obj = cmds.polyCube()
+        elif mode == 'Sphere':
+            obj = cmds.polySphere()
+        elif mode == 'Cone':
+            obj = cmds.polyCone()
+        elif mode == 'Cylinder':
+            obj = cmds.polyCylinder()
+        else:
+            cmds.error("I dont know what to create")
+
+        objList.append(obj[0])
+        
+    return objList
+   
+def randomize(objList=None, minValue=0, maxValue=50):
+    
+    if objList is None:
+        objList = cmds.ls(sl=True)
+    
+    for obj in objList:
+        cmds.setAttr(obj+'.tx', random.randint(minValue, maxValue))
+        cmds.setAttr(obj+'.ty', random.randint(minValue, maxValue))
+        cmds.setAttr(obj+'.tz', random.randint(minValue, maxValue))
