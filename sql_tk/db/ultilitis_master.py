78ff17ed7f4d7da9f78023b503eaabf4dde327e4
panda <damgteam@gmail.com>
2017-12-02 18:57:24 +1300

    update readme

--- sql_tk/db/ultilitis_master.py
+++ sql_tk/db/ultilitis_master.py
@@ -19,6 +19,7 @@ logger = logging.getLogger(__file__)
 logger.setLevel(logging.DEBUG)
 
 table_bk_pth = os.path.join(os.getenv('PIPELINE_TOOL'), 'sql_tk/_bk/table.map.yml')
+
 table_info = {}
 conn = lite.connect('meta_data.db')
 c = conn.cursor()
@@ -28,81 +29,63 @@ table_timelog = 'timelog'
 table_local_pc = 'local_pc'
 table_pipeline = 'pipelineCf'
 table_current_user = 'current_user'
-varchar = 'VARCHAR(20)'
 
-def create_table_with_4_column(table_name, new_field, field_type):
+def create_table_pipeline_config(nf1, nf2, nf3, table_name=table_pipeline):
 
-    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} {ft1}, {nf2} {ft2}, {nf3} {ft3}, {nf4} {ft4".
-              format(tn=table_name, nf1=new_field[0], ft1=field_type[0], nf2=new_field[1],
-                     ft2=field_type[1], nf3=new_field[2], ft3=field_type[2],
-                     ft4=field_type[3], nf4=field_type[3]))
-
-    conn.commit()
-    datetime_stamp = func.create_date_time_stamp()
-    table_info[datetime_stamp] = [table_name, new_field, field_type]
-    func.dataHandle('yaml', 'a+', table_bk_pth, table_info)
-    logger.info('%s table created' % table_name)
-    return True
-
-def create_table_with_5_column(table_name, new_field, field_type):
-    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} {ft1}, {nf2} {ft2}, {nf3} {ft3}, {nf4} {ft4}, {nf5} {ft5})".
-              format(tn=table_name, nf1=new_field[0], ft1=field_type[0], nf2=new_field[1], ft2=field_type[1],
-                     nf3=new_field[2], ft3=field_type[2], nf4=new_field[3], ft4=field_type[3],
-                     nf5=new_field[4], ft5=field_type[4]))
+    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} TEXT, {nf2} TEXT, {nf3} TEXT, maya TEXT, "
+              "max3ds TEXT, zbrush TEXT, mudbox TEXT, houdini TEXT, "
+              "mari TEXT, substance TEXT, nukex TEXT, hiero TEXT, after_effect TEXT, "
+              "premiere pro  TEXT, photoshop TEXT)".format(tn=table_name, nf1=nf1, nf2=nf2, nf3=nf3))
 
     conn.commit()
-    datetime_stamp = func.create_date_time_stamp()
-    table_info[datetime_stamp] = [table_name, new_field, field_type]
-    func.dataHandle('yaml', 'a+', table_bk_pth, table_info)
-    logger.info('%s table created' % table_name)
-
-    return True
-
-def create_table_user_profile_by_unix(unix):
-    c.execute("CREATE TABLE IF NOT EXISTS {tn} (class VARCHAR(20), title VARCHAR(20), lastname VARCHAR(20), "
-              "firstname VARCHAR(20), avatar VARCHAR(20), time_stamp VARCHAR(20), date_stamp VARCHAR(20), "
-              "remember_login INT, maya INT, max3ds INT, zbrush INT, mudbox INT, houdini INT, mari INT, substance INT, "
-              "nukex INT, hiero INT, after_effects INT, premiere_pro INT, photoshop INT,illustrator INT, pycharm INT, "
-              "sublime INT)".format(tn=unix))
-
-    conn.commit()
-    datetime_stamp = func.create_date_time_stamp()
-    table_info[datetime_stamp] = [unix, 'class', 'title', 'lastname', 'firstname', 'avatar', 'time_stamp', 'date_stamp',
-                                  'remember_login', 'maya', 'max3ds', 'zbrush', 'mudbox', 'houdini', 'mari',
-                                  'substance',
-                                  'nukex', 'hiero', 'after_effects', 'premiere_pro', 'photoshop', 'illustrator']
-    func.dataHandle('yaml', 'a+', table_bk_pth, table_info)
-    logger.info('table: %s created' % unix)
-    return True
-
-def create_table_pipeline_config(table_name, new_field, field_type):
-    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} {ft1}, {nf2} {ft2}, {nf3}{ft3}, maya VARCHAR(20), "
-              "max3ds VARCHAR(20), zbrush VARCHAR(20), mudbox VARCHAR(20), houdini VARCHAR(20), "
-              "mari VARCHAR(20), substance VARCHAR(20), nukex VARCHAR(20), hiero VARCHAR(20), ae VARCHAR(20), "
-              "pr VARCHAR(20), pts VARCHAR(20), illus VARCHAR(20), pycharm VARCHAR(20), sublime VARCHAR(20))".
-              format(tn=table_name, nf1=new_field[0], ft1=field_type[0], nf2=new_field[1], ft2=field_type[1],
-                     nf3=new_field[2], ft3=field_type[2]))
-
-    conn.commit()
-    datetime_stamp = func.create_date_time_stamp()
-    table_info[datetime_stamp] = [ table_name, new_field, field_type,
-                                  'maya', 'max3ds', 'zbrush', 'mudbox', 'houdini', 'mari', 'substance', 'nukex',
-                                  'hiero', 'after_effects', 'premiere_pro', 'photoshop', 'illustrator']
-
-    func.dataHandle('yaml', 'a+', table_bk_pth, table_info)
 
     logger.info('table: %s created' % table_name)
-    return True
 
-def create_table_current_login_user(table_name, unix, token, username):
-    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} {ft1} , {nf2} {ft2}, {nf3} {ft3})".
-              format(tn=table_name, nf1=unix, ft1=varchar, nf2=token, ft2=varchar, nf3=username, ft3=varchar, ))
+def create_table_with_4_column(nf1, nf2, nf3, nf4, table_name=table_login):
+
+    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} TEXT, {nf2} TEXT, {nf3} TEXT, {nf4} TEXT);".format(
+        tn=table_name, nf1=nf1, nf2=nf2, nf3=nf3, nf4=nf4))
+
     conn.commit()
+    logger.info('%s table created' % table_name)
+
+def create_table_with_5_column(nf1, nf2, nf3, nf4, nf5, table_name):
+    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} TEXT, {nf2} TEXT, {nf3} TEXT, {nf4} TEXT, "
+              "{nf5} TEXT)".format(tn=table_name, nf1=nf1, nf2=nf2, nf3=nf3, nf4=nf4, nf5=nf5))
+
+    conn.commit()
+
+    logger.info('%s table created' % table_name)
+
+def create_table_user_profile_by_unix(table_name):
+    c.execute("CREATE TABLE IF NOT EXISTS {tn} (title TEXT, lastname TEXT,firstname TEXT, avatar TEXT, "
+              "time_stamp TEXT, date_stamp TEXT, remember_login INT, maya INT, max3ds INT, zbrush INT, mudbox INT, "
+              "houdini INT, mari INT, substance INT, nukex INT, hiero INT, after_effects INT, premiere_pro INT, "
+              "photoshop INT, illustrator INT )".format(tn=table_name))
+
+    conn.commit()
+
+    logger.info('table: %s created' % table_name)
+
+def create_table_current_login_user(unix, token, username, table_name=table_current_user):
+    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} TEXT , {nf2} TEXT, {nf3} TEXT)".
+              format(tn=table_name, nf1=unix, nf2=token, nf3=username))
+
+    conn.commit()
+
     logger.info('server_querry table created')
+
+def delete_all_database_from_table(table_name=table_current_user):
+    # Delete old data first
+    c.execute("SELECT * FROM {tn}".format(tn=table_name))
+    rows = c.fetchall()
+    logger.info('Clean old data')
+    c.execute("DELETE FROM {tn}".format(tn=table_name))
+    conn.commit()
     return True
 
-def query_info_from_username(username):
-    c.execute("SELECT * FROM {tn}".format(tn=table_login))
+def query_info_from_username(username, table_name=table_login):
+    c.execute("SELECT * FROM {tn}".format(tn=table_name))
     rows = c.fetchall()
     unix = [row[0] for row in rows if row[2] == username]
     token = [row[1] for row in rows if row[2] == username]
@@ -111,27 +94,174 @@ def query_info_from_username(username):
 
 def check_password_log_in(username, password):
     input = func.encoding(password)
-    unix, token, match = query_info_from_username(username)
+    table_name = table_login
+    unix, token, match = query_info_from_username(table_name, username)
     if input == match:
         returning = "PASS"
     else:
         returning = "DENY"
     return returning
 
+def query_unix_used_list_from_unix(unix, table_name=table_login):
+    """
+    use login table
 
-def dynamic_user_account_entry(table_name, unix, token, username, password):
+    """
+    c.execute("SELECT unix FROM {tn}".format(tn=table_name))
+    rows = c.fetchal()
+    unix_list = [row[0] for row in rows]
+    unix_index = unix_list.index(unix)
+    return unix_index, unix_list
 
-    c.execute("INSERT INTO TABLE {tn} ({dt1},{dt2},{dt3},{dt4}) VALUES (?, ?, ?, ?)), "
-              "(unix, token, username, password)".format(tn=table_name, dt1=unix, dt2=token, dt3=username, dt4=password))
+def query_token_list_from_token(token, table_name=table_login):
+    """
+    use login table
 
+    """
+    c.execute("SELECT token FROM {tn}".format(table_name))
+    rows = c.fetchall()
+    token_list = [row[0] for row in rows]
+    token_index = token_list.index(token)
+    return token_index, token_list
+
+def query_token_list_from_unix(unix, table_name=table_login):
+    """
+    use login table
+
+    """
+    unix_index, unix_list = query_unix_used_list_from_unix(unix)
+    c.execute("SELECT token FROM {tn}".format(tn=table_name))
+    rows = c.fetchall()
+    token_list = [row[0] for row in rows]
+    token = token_list[unix_index]
+    token_index = unix_index
+    return token_index, token, token_list
+
+def query_token_list_and_unix_list(unix):
+    unix_index, unix_list = query_unix_used_list_from_unix(unix)
+    token_index, token, token_list = query_token_list_from_unix(unix)
+    index = token_index
+    return index, token, unix_list, token_list
+
+def query_token_from_unix(unix):
+    index, token, unix_list, token_list = query_token_list_and_unix_list(unix)
+    return index, token
+
+def query_product_id_from_unix(unix, table_name=table_local_pc):
+    index, token = query_token_from_unix(unix)
+    c.execute("SELECT product_id FROM {tn}".format(table_name))
+    rows = c.fetchall()
+    product_id_list = [row[0] for row in rows]
+    product_id = product_id_list[index]
+    return product_id
+
+def query_product_id_from_token(token, table_name=table_local_pc):
+    c.execute("SELECT token, product_id FROM {tn}".format(table_name))
+    rows = c.fetchall()
+    product_id_list = [row[0] for row in rows]
+    token_index, token_list = query_token_list_from_token(token)
+    product_id = product_id_list[token_index]
+    return product_id
+
+def query_value_login_remember_from_unix(unix):
+    c.execute("SELECT * FROM {tn}".format(unix))
+    rows = c.fetchall()
+    index = 7
+    for row in rows:
+        value_setting_remember = row[7]
+
+    return value_setting_remember
+
+def query_local_info_pc_from_unix(unix):
+    c.execute("SELECT * FROM local_pc")
+    rows = c.fetchall()
+    local_pc_info = [row for row in rows if row[0] == unix]
+    return local_pc_info
+
+def dynamic_user_account_entry(dt1, dt2, dt3, dt4, table_name=table_login):
+    password = func.encoding(dt4)
+    c.execute("INSERT INTO {tn} (unix, token, username, password) VALUES (?,?,?,?)".
+              format(tn=table_name),(dt1, dt2, dt3, password))
     conn.commit()
-    logger.info('inserted to table %s' % table_name)
+    logger.info('inserted to table loginCf')
+
+def dynamic_update_current_user(unix, token, username, table_name=table_current_user):
+    c.execute("INSERT INTO {tn} (unix,token,username) VALUES (?,?,?)".format(tn=table_name),(unix, token, username))
+
+def dynamic_new_user_entry(unix, class_, title, firstname, lastname, avatar, remember):
+    time_stamp = func.create_time_stamp()
+    date_stamp = func.create_date_stamp()
+    c.execute("INSERT INTO {tn} (class_, title, lastname, firstname, avatar, time_stamp, date_stamp, remember) "
+       "VALUES (?, ?, ?, ?, ?, ?, ?, ?)".format(tn=unix),(class_, title, firstname, lastname, avatar, time_stamp,
+                                                          date_stamp, remember))
+    conn.commit()
+
+def dynamic_update_password_by_user(unix, new_password, table_name=table_login):
+    password_new = func.encoding(new_password)
+    c.execute("SELECT * FROM {tn}".format(table_name))
+    rows = c.fetchall()
+    c.execute("UPDATE {tn} Set password = (?) WHERE unix = (?)".format(tn=table_name), (password_new, unix))
+    conn.commit()
+
+def dynamic_new_login_remember(unix, value):
+    c.execute("INSERT INTO {tn} (remember_login) VALUES (?))".format(tn=unix), (value))
+    conn.commit()
     return True
 
-def dynamic_update_current_login_account(table_name, unix, token, username):
-    # Delete old data first
-    c.execute("SELECT * FROM {tn}".format(tn=table_name))
-    rows = c.fetchall()
+def dynamic_new_timelog_entry(unix, log_action, table_name=table_timelog):
+    datelog = func.create_date_stamp()
+    timelog = func.create_time_stamp()
+
+    c.execute("INSERT INTO {tn} (datelog, unix, log_action, timelog) VALUES (?, ?, ?, ?,))".format(tn=table_name),
+       (datelog, unix, log_action, timelog))
+
+    conn.commit()
+    return True
+
+def dynamic_add_user_profile_entry(title, lastname, firstname, avatar, time_stamp, date_stamp, table_name):
+    c.execute("INSERT INTO {tn} (title, lastname, firstname, avatar, time_stamp, date_stamp, remember_login) VALUES (?,"
+        "?,?,?,?,?,?,?)".format(tn=table_name),(title, lastname, firstname, avatar, time_stamp, date_stamp))
+
+    conn.commit()
+
+    logger.info('table: %s created' % table_name)
+
+def dynamic_pipeline_config_entry(unix, trackKeys, table_name=table_pipeline):
+    softs_list = ['maya', '3ds Max', 'ZBrush', 'Mudbox', 'Houdini', 'Mari', 'Substance', 'NukeX', 'Hiero',
+                     'After Effects', 'Premiere Pro', 'Photoshop', 'Illustrator', 'Pycharm', 'Sublime Text']
+    values = {}
+    token = query_token_from_unix(unix)
+    product_id = query_product_id_from_unix(unix)
+    for real_key in trackKeys:
+        for check_key in softs_list:
+            if check_key in real_key:
+                values[check_key] = 1
+            else:
+                values[check_key] = 0
+
+    maya = values['maya']
+    max3ds = values['3ds Max']
+    zbrush = values['ZBrush']
+    mudbox = values['Mudbox']
+    houdini = values['Houdini']
+    mari = values['Mari']
+    substance = values['Substance']
+    nukex = values['NukeX']
+    hiero = values['Hiero']
+    ae = values['AfterEffects']
+    pr = values['Primiere Pro']
+    pts = values['Photoshop']
+    illus = values['Illustrator']
+    pycharm = values['Pycharm']
+    sublime = values['Sublime']
 
 
+    c.execute("INSERT INTO {tn} (unix, token, product_id, maya, max3dds, zbrush, mudbox, houdini, mari, substance, "
+       "nukex, hiero, ae, pr, pts, illus, pycharm, sublime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, "
+       "?, ?, ?, ?, ?, ?, ?, ?, ?, ?)".format(tn=table_name), (unix, token, product_id, maya, max3ds, zbrush, mudbox, houdini, mari,
+                                         substance, nukex, hiero, ae, pr, pts, illus, pycharm, sublime))
 
+    conn.commit()
+
+def connect_to_core_data():
+    core = lite.connect('core.db')
--- sql_tk/db/ultilitis_user.py
+++ sql_tk/db/ultilitis_user.py
@@ -8,10 +8,9 @@ Description:
 
 """
 
-import datetime
 import logging
+import os
 import sqlite3 as lite
-import time
 
 from sql_tk.db import ultilitis_master as umaster
 from tk import appFuncs as func
@@ -28,173 +27,56 @@ table_login = 'loginCf'
 table_timelog = 'timelog'
 table_local_pc = 'local_pc'
 table_pipeline = 'pipelineCf'
+table_current_user = 'current_user'
+tlf = ['date_log', 'unix', 'log_action', 'time_log']
+tcu = ['unix', 'token', 'username']
+linf = ['unix', 'token', 'username', 'password']
 
-#--------------------------------------------------------
-# CREATE TABLE
-def create_table_new_user(username, password):
-    table_name = table_login
-    new_field1 = ('unix', 'token', 'username', 'password')
-    field_type1 = ('VARCHAR(20)', 'VARCHAR(20)', 'VARCHAR(20)', 'VARCHAR(20)')
-    unix, token, time_stamp, date_stamp = func.generate_set_unix_id()
-    umaster.create_table_with_4_column(table_name, new_field1, field_type1)
-    umaster.create_table_user_profile_by_unix(table_name=unix)
-    umaster.dynamic_user_account_entry(table_name, unix, token, username, password)
-    # Create timelog table
-    new_field2 = ('datelog', 'unix', 'log_action', 'timelog')
-    umaster.create_table_with_4_column('timelog', new_field2, field_type1)
-    # Create pipeline config table
-    new_field3 = ('unix', 'token', 'product_id')
-    field_type = ('VARCHAR(20)', 'VARCHAR(20)', 'VARCHAR(20)')
-    umaster.create_table_pipeline_config(table_pipeline, new_field3, field_type)
+class CreateNewUser(object):
 
-#--------------------------------------------------------
-# FROM USER
+    def __init__(self, username, password, title, lastname, firstname, avatar):
+        super(CreateNewUser, self).__init__()
 
+        password = func.encoding(password)
 
+        self.username = username
+        self.password = password
 
+        self.create_timelog_table()
 
-def update_password_by_user(unix, new_password):
-    password_new = func.encoding(new_password)
-    ce("SELECT * FROM loginCf")
-    # rows = c.fetchall()
-    ce("UPDATE loginCf Set password = (?) WHERE unix = (?)", (password_new, unix))
-    conn.commit()
+        self.create_current_login_table()
 
-def dynamic_new_login_user_entry(unix, username, password):
-    password = func.encoding(password)
-    token = func.create_new_unique_id()
-    ce("INSERT INTO accountCf (unix, token, username, password) VALUES (?, ?, ?, ?)",
-       (unix, token, username, password))
-    conn.commit()
-    return True
+        self.create_login_account_table()
 
-def dynamic_new_user_entry(unix, class_, title, lastname, firstname, avatar):
-    create_time = str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y.%m.%d %H:%M:%S'))
-    ce("INSERT INTO accountCf (unix, class, title, firstname, lastname, avatar, create_time) "
-       "VALUES (?, ?, ?, ?, ?, ?, ?)", (unix, class_, title, firstname, lastname, avatar, create_time))
-    conn.commit()
-    return True
+    def create_timelog_table(self):
+        # Create login profile
+        umaster.create_table_with_4_column(tlf[0], tlf[1], tlf[2], tlf[3], table_timelog)
 
-def dynamic_new_login_remember(unix, value):
-    ce("INSERT INTO settingCf (unix, remember_login) VALUES (?, ?))", (unix, value))
-    conn.commit()
-    return True
+    def create_current_login_table(self):
+        # Create table current login user
+        umaster.create_table_current_login_user(tcu[0], tcu[1], tcu[2], table_current_user)
 
-def dynamic_new_profile_user_entry(username, password, class_, title, lastname, firstname, avatar):
-    password = func.encoding(password)
-    unix = create_new_unix_value()
-    dynamic_new_login_user_entry(unix, username, password)
-    dynamic_new_user_entry(unix, class_, title, lastname, firstname, avatar)
-    return True
+    def create_login_account_table(self):
+        # Create user account log in table
+        umaster.create_table_with_4_column(linf[0], linf[1], linf[2], linf[3], table_login)
 
-def dynamic_new_timelog_entry(unix, log_action):
-    datelog = func.proc('date')
-    timelog = func.proc('time')
-    ce("INSERT INTO timelog (datelog, unix, log_action, timelog) VALUES (?, ?, ?, ?,))",
-       (datelog, unix, log_action, timelog))
-    conn.commit()
-    return True
+    def generate_unix_and_token_for_account(self):
+        # Create unix id and token
+        info = {}
+        self.unix, self.token, self.datelog, self.timelog = func.generate_set_unix_id()
+        info[self.username] = [self.datelog, self.timelog]
+        pth = os.path.join(os.getenv('PIPELINE_TOOL'), 'sql_tk/_bk')
+        filename = '%s.%s.yml' % (self.unix, self.token)
+        backupPth = os.path.join(pth, filename)
+        func.dataHandle('yaml', 'w', backupPth, info)
 
-def dynamic_pipeline_config_entry(unix, trackKeys):
-    softs_list = ['maya', '3ds Max', 'ZBrush', 'Mudbox', 'Houdini', 'Mari', 'Substance', 'NukeX', 'Hiero',
-                     'After Effects', 'Premiere Pro', 'Photoshop', 'Illustrator', 'Pycharm', 'Sublime Text']
-    values = {}
-    token = query_token_from_unix(unix)
-    product_id = query_product_id_from_unix(unix)
-    for real_key in trackKeys:
-        for check_key in softs_list:
-            if check_key in real_key:
-                values[check_key] = 1
-            else:
-                values[check_key] = 0
+        return self.unix, self.token, self.datelog, self.timelog
 
-    maya = values['maya']
-    max3ds = values['3ds Max']
-    zbrush = values['ZBrush']
-    mudbox = values['Mudbox']
-    houdini = values['Houdini']
-    mari = values['Mari']
-    substance = values['Substance']
-    nukex = values['NukeX']
-    hiero = values['Hiero']
-    ae = values['AfterEffects']
-    pr = values['Primiere Pro']
-    pts = values['Photoshop']
-    illus = values['Illustrator']
-    pycharm = values['Pycharm']
-    sublime = values['Sublime']
+    def set_up_new_user_account(self):
+        # Create user login account
+        umaster.dynamic_user_account_entry(self.unix, self.token, self.username, self.password)
+        # Create profile table base on unix id
+        umaster.create_table_user_profile_by_unix(table_name=self.my_unix)
+        # Update profile and setting
+        umaster.dynamic_add_user_profile_entry(self.title, self.lastname, self.firstname, self.avatar, self.datelog, self.timelog, self.unix)
 
-
-    ce("INSERT INTO pipelineCf (unix, token, product_id, maya, max3dds, zbrush, mudbox, houdini, mari, substance, "
-       "nukex, hiero, ae, pr, pts, illus, pycharm, sublime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, "
-       "?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (unix, token, product_id, maya, max3ds, zbrush, mudbox, houdini, mari,
-                                         substance, nukex, hiero, ae, pr, pts, illus, pycharm, sublime))
-    conn.commit()
-
-#--------------------------------------------------------
-# QUERY DATA
-def query_unix_used_list_from_unix(unix):
-    ce("SELECT unix FROM loginCf")
-    rows = ce.fetchal()
-    unix_list = [row[0] for row in rows]
-    unix_index = unix_list.index(unix)
-    return unix_index, unix_list
-
-def query_token_list_from_token(token):
-    ce("SELECT token FROM loginCf")
-    rows = c.fetchall()
-    token_list = [row[0] for row in rows]
-    token_index = token_list.index(token)
-    return token_index, token_list
-
-def query_token_list_from_unix(unix):
-    unix_index, unix_list = query_unix_used_list_from_unix(unix)
-    ce("SELECT token FROM loginCf")
-    rows = c.fetchall()
-    token_list = [row[0] for row in rows]
-    token = token_list[unix_index]
-    token_index = unix_index
-    return token_index, token, token_list
-
-def query_token_list_and_unix_list(unix):
-    unix_index, unix_list = query_unix_used_list_from_unix(unix)
-    token_index, token, token_list = query_token_list_from_unix(unix)
-    index = token_index
-    return index, token, unix_list, token_list
-
-def query_token_from_unix(unix):
-    index, token, unix_list, token_list = query_token_list_and_unix_list(unix)
-    return index, token
-
-def query_product_id_from_unix(unix):
-    index, token = query_token_from_unix(unix)
-    ce("SELECT product_id FROM local_pc")
-    rows = c.fetchall()
-    product_id_list = [row[0] for row in rows]
-    product_id = product_id_list[index]
-    return product_id
-
-def query_product_id_from_token(token):
-    ce("SELECT token, product_id FROM local_pc")
-    rows = c.fetchall()
-    product_id_list = [row[0] for row in rows]
-    token_index, token_list = query_token_list_from_token(token)
-    product_id = product_id_list[token_index]
-    return product_id
-
-def query_value_login_remember_from_unix(unix):
-    ce("SELECT * FROM settingCf")
-    rows = c.fetchall()
-    unix_list = [row[0] for row in rows]
-    value_list = [row[1] for row in rows]
-    value_setting_remember = value_list[unix_list.index(unix)]
-    return value_setting_remember
-
-def query_local_info_pc_from_unix(unix):
-    ce("SELECT * FROM local_pc")
-    rows = c.fetchall()
-    local_pc_info = [row for row in rows if row[0] == unix]
-    return local_pc_info
-
-#--------------------------------------------------------
-# INSERT DATA
