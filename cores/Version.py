613fe6708e4c8a16b4115bc9b6019eb2a0765d81
Trinh Do <dot@damgteam.com>
2019-11-06 03:28:14 +1300

    update action manager, fix signal emit twice bug.

--- cores/Version.py
+++ cores/Version.py
@@ -15,22 +15,23 @@ from damg import DAMGTUPLE
 
 from appData import APP_DATA_DIR
 
+with open(os.path.join(APP_DATA_DIR, 'metadatas.py'), "rb") as f:
+    metadata = f.read().decode('utf-8')
+
 class __version_info__(DAMGTUPLE):
 
     Type                            = 'DAMGVERSIONINFO'
     key                             = '__version_info__'
 
-    with open(os.path.join(APP_DATA_DIR, 'metadatas.py'), "rb") as f:
-        metadata = f.read().decode('utf-8')
+    def __new__(self, metadata):
 
-    def __new__(self):
+        self.metadata = metadata
 
         self._MAJOR = self.parse(__version_info__, r'__majorVersion__\s+=\s+(.*)')
         self._MINOR = self.parse(__version_info__, r'__minorVersion__\s+=\s+(.*)')
         self._MICRO = self.parse(__version_info__, r'__microVersion__\s+=\s+(.*)')
 
         return tuple.__new__(__version_info__, (self._MAJOR, self._MINOR, self._MICRO))
-
 
     def __bases__(self):
         return tuple(__version_info__, tuple(self.major_version, self.minor_version, self.micro_version))
