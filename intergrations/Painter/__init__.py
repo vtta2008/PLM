4040f74d5dc529071dd0618c7aaa3228debbdff2
LilyHubby <damgteam@gmail.com>
2020-03-16 06:08:25 +1300

    reconfig all paths

---
+++ intergrations/Painter/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+
+Script Name: __init__.py.py
+Author: Do Trinh/Jimmy - 3D artist.
+
+Description:
+    
+
+"""
+# -------------------------------------------------------------------------------------------------------------
+from __future__ import absolute_import, unicode_literals
+
+# -------------------------------------------------------------------------------------------------------------
+# Created by panda on 1/20/2020 - 5:34 AM
+# © 2017 - 2019 DAMGteam. All rights reserved
