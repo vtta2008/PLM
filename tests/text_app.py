4dcca2bf280e600aaee0b35b4d8915d056b9a1a2
LilyHubby <damgteam@gmail.com>
2020-03-26 06:26:33 +1300

    fixing splash ui

--- tests/text_app.py
+++ tests/text_app.py
@@ -15,7 +15,7 @@ from .context import PLM
 
 def test_app(capsys, example_fixture):
     # pylint: disable=W0612,W0613
-    PLM.app.run()
+    PLM.PLM.run()
     captured = capsys.readouterr()
 
     assert "Hello World..." in captured.out
