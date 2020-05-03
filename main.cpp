9e54068fb53485249363d6083f4cecc5b35459b9
LilyHubby <damgteam@gmail.com>
2020-05-04 05:13:52 +1200

    fixing splash ui

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <Windows.h>
+#include <string>
 
 using namespace std;
+
+
+string ExePath()
+{
+
+    char buffer[MAX_PATH];
+    GetModuleFileName( NULL, buffer, MAX_PATH );
+    string::size_type pos = string( buffer ).find_last_of( "\\/" );
+    return string( buffer ).substr( 0, pos);
+
+}
 
 int main()
 {
 
-    ShowWindow(GetConsoleWindow(), SW_HIDE);
-    // Start project
+    //ShowWindow(GetConsoleWindow(), SW_HIDE);
+    cout << ExePath() << "\n";
     system("python -m PLM");
-    ShowWindow(GetConsoleWindow(), SW_RESTORE);
 
 };
